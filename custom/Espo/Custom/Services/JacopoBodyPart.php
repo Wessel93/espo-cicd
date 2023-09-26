86780cc6219e96e4cbf84402f5ea3126c94014b1
root <root@espo-cicd-test.spzvc1l4xlfernz5g1nuuwo1yd.ax.internal.cloudapp.net>
2023-09-26 13:35:11 +0000

    commit by jmargutt

---
+++ custom/Espo/Custom/Services/JacopoBodyPart.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace Espo\Custom\Services;
+
+class JacopoBodyPart extends \Espo\Core\Templates\Services\Base
+{
+}
---
+++ custom/Espo/Custom/Services/TijsBodyPart.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace Espo\Custom\Services;
+
+class TijsBodyPart extends \Espo\Core\Templates\Services\Base
+{
+}
