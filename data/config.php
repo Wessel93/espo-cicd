03b86e936d2631f654b84cbed4d1ea23d8fd186f
root <root@espo-cicd-test.spzvc1l4xlfernz5g1nuuwo1yd.ax.internal.cloudapp.net>
2023-10-03 13:53:15 +0000

    commit by tijsziere

--- data/config.php
+++ data/config.php
@@ -283,8 +283,8 @@ return [
     1 => 'email',
     2 => 'phone'
   ],
-  'cacheTimestamp' => 1696339788,
-  'microtime' => 1696339788.003414,
+  'cacheTimestamp' => 1696341149,
+  'microtime' => 1696341149.687155,
   'siteUrl' => 'http://10.4.0.14',
   'fullTextSearchMinLength' => 4,
   'appTimestamp' => 1695739336,
